512a8552bd24a71b0cb401ab34b160064a333946
Gabriel Soarde <133424939+GabrielSoarde@users.noreply.github.com>
2024-05-08 11:29:11 -0300

    Add files via upload

---
+++ src/main/java/com/soardev/userdept/entities/Department.java
@@ -0,0 +1,27 @@
+package com.soardev.userdept.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="tb_department")
+public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public Department() {
+    }
+    public Long getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
